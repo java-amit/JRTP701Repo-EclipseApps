f69655a2a118f2e70db27abd25c01f5e7f11bfc4
amit <amitbh94@gmail.com>
2025-10-27 22:03:36 +0530

    Initial Commit for Logging Application Slf4j

---
+++ BootLoggingProj01/src/main/java/com/nt/BootLoggingProj01Application.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLoggingProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootLoggingProj01Application.class, args);
+	}
+
+}
