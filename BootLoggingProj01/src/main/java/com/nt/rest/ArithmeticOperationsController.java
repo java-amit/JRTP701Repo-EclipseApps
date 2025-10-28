ad1432b75e2e38fb2d7cc2aae30e7aa17d2f0326
amit <amitbh94@gmail.com>
2025-10-28 15:49:01 +0530

    Second Distributed application or Restful WebServices is created:ArithmeticOperationsController

---
+++ BootLoggingProj01/src/main/java/com/nt/rest/ArithmeticOperationsController.java
@@ -0,0 +1,29 @@
+package com.nt.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class ArithmeticOperationsController {
+
+	@GetMapping("/div")
+	public String div() {
+		log.info("At begning of div() method");
+		
+		try {
+			log.info("performing arithmatic operaion");
+		float result = 100/0.0f;
+		log.info("Arithmetic operation completed sucessfully");
+		return "result is::"+result;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			log.error("Problem in Arithmetic operation::"+e.getMessage());
+			return "problem:"+e.getMessage();
+		}
+	}
+	
+}
